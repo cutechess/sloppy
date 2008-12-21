4414f9e0d345610e2487656ca5bfefdbe0f05a6e
Ilari Pihlajisto <ilari.pihlajisto@mbnet.fi>
2008-12-21 19:04:46 +0200

    Get rid of bench.txt, use hard-coded positions instead.

--- bench.c
+++ bench.c
@@ -28,25 +28,6 @@
 #include "bench.h"
 
 
-/* Get the number of lines in a text file.  */
-static int
-get_nlines(FILE *fp)
-{
-	int c;
-	int nlines;
-	
-	ASSERT(1, fp != NULL);
-
-	nlines = 0;
-	while ((c = fgetc(fp)) != EOF && c != '\0') {
-		if(c == '\n')
-			++nlines;
-	}
-	fseek(fp, 0, SEEK_SET);
-
-	return nlines;
-}
-
 /* Run a test position (eg. WAC, ECM, WCSAC).
 
    Format of pos: <FEN> <"bm" or "am"> <move>; <position id>;
@@ -177,6 +158,34 @@ test_suite(Chess *chess, const char *filename)
 	printf("\n%d of %d tests were solved.\n", nsolved, nsolved + nfailed);
 }
 
+static const char *bench_fen[] =
+{
+	"1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - 0 1",
+	"r1k5/4npp1/1ppr3p/p6P/P2PPPP1/1NR5/5K2/2R5 w - - 0 1",
+	"q1rr1k2/3bbnnp/p2p1pp1/2pPp3/PpP1P1P1/1P2BNNP/2BQ1PRK/7R b - - 0 1",
+	"nbqkb1r1/p3pppp/1p6/2ppP3/3N4/2P5/PPP1QPPP/R1B1KB1R w KQ - 0 1",
+	"r1b2rk1/2q1b1pp/p2ppn2/1p6/3QP3/1BN1B3/PPP3PP/R4RK1 w - - 0 1",
+	"2r3k1/pppR1pp1/4p3/4P1P1/5P2/1P4K1/P1P5/8 w - - 0 1",
+	"1nk1r1r1/pp2n1pp/4p3/q2pPp1N/b1pP1P2/B1P2R2/2P1B1PP/R2Q2K1 w - - 0 1",
+	"4b3/p3kp2/6p1/3pP2p/2pP1P2/4K1P1/P3N2P/8 w - - 0 1",
+	"2kr1bnr/pbpq4/2n1pp2/3p3p/3P1P1B/2N2N1Q/PPP3PP/2KR1B1R w - - 0 1",
+	"3rr1k1/pp3pp1/1qn2np1/8/3p4/PP1R1P2/2P1NQPP/R1B3K1 b - - 0 1",
+	"2r1nrk1/p2q1ppp/bp1p4/n1pPp3/P1P1P3/2PBB1N1/4QPPP/R4RK1 w - - 0 1",
+	"r3r1k1/ppqb1ppp/8/4p1NQ/8/2P5/PP3PPP/R3R1K1 b - - 0 1",
+	"r2q1rk1/4bppp/p2p4/2pP4/3pP3/3Q4/PP1B1PPP/R3R1K1 w - - 0 1",
+	"rnb2r1k/pp2p2p/2pp2p1/q2P1p2/8/1Pb2NP1/PB2PPBP/R2Q1RK1 w - - 0 1",
+	"2r3k1/1p2q1pp/2b1pr2/p1pp4/6Q1/1P1PP1R1/P1PN2PP/5RK1 w - - 0 1",
+	"r1bqkb1r/4npp1/p1p4p/1p1pP1B1/8/1B6/PPPN1PPP/R2Q1RK1 w kq - 0 1",
+	"r1bq1rk1/pp2ppbp/2np2p1/2n5/P3PP2/N1P2N2/1PB3PP/R1B1QRK1 b - - 0 1",
+	"3rr3/2pq2pk/p2p1pnp/8/2QBPP2/1P6/P5PP/4RRK1 b - - 0 1",
+	"r4k2/pb2bp1r/1p1qp2p/3pNp2/3P1P2/2N3P1/PPP1Q2P/2KRR3 w - - 0 1",
+	"3rn2k/ppb2rpp/2ppqp2/5N2/2P1P3/1P5Q/PB3PPP/3RR1K1 w - - 0 1",
+	"2r2rk1/1bqnbpp1/1p1ppn1p/pP6/N1P1P3/P2B1N1P/1B2QPP1/R2R2K1 b - - 0 1",
+	"r1bqk2r/pp2bppp/2p5/3pP3/P2Q1P2/2N1B3/1PP3PP/R4RK1 b kq - 0 1",
+	"r2qnrnk/p2b2b1/1p1p2pp/2pPpp2/1PP1P3/PRNBB3/3QNPPP/5RK1 w - - 0 1",
+	"r2q1rk1/1ppnbppp/p2p1nb1/3Pp3/2P1P1P1/2N2N1P/PPB1QP2/R1B2RK1 b - - 0 1"
+};
+
 /* Benchmark Sloppy's speed, branching factor, and hash table efficiency.  */
 #define BENCH_FILE "bench.txt"
 void
@@ -186,39 +195,31 @@ bench(void)
 	SearchData sd;
 	S64 timer;
 	int npos = 0;
-	int nlines;
+	int nfen = (int)(sizeof(bench_fen) / sizeof(char*));
 	
-	char fen[MAX_BUF];
-	FILE *fp;
-
 	double avg_bfactor;
 	double t_elapsed;
 	double hhit_rate;
 	U64 nnodes_all;	/* num. of all nodes (main + qs) */
 	int nps;	/* nodes (all types) per second */
-
+	int i;
+	
 	init_chess(&chess);
 	init_search_data(&sd);
 	chess.max_depth = 8;
 	chess.increment = 60000;
 
-	if ((fp = fopen(BENCH_FILE, "r")) == NULL) {
-		my_perror("Can't open file %s", BENCH_FILE);
-		return;
-	}
-
-	nlines = get_nlines(fp);
 	printf("Running benchmark at search depth %d...\n", chess.max_depth);
 	timer = get_ms();
-	progressbar(nlines, 0);
-	while (fgetline(fen, MAX_BUF, fp) != EOF) {
+	progressbar(nfen, 0);
+	for (i = 0; i < nfen; i++) {
+		const char *fen = bench_fen[i];
 		if (strlen(fen) <= 1)
 			continue;
 		if (!fen_to_board(&chess.board, fen)) {
 			id_search(&chess, NULLMOVE);
 			if (chess.sd.cmd_type != CMDT_CONTINUE) {
 				printf("Benchmark cancelled by user\n");
-				my_close(fp, BENCH_FILE);
 				return;
 			}
 
@@ -228,11 +229,10 @@ bench(void)
 			sd.nhash_hits += chess.sd.nhash_hits;
 			sd.bfactor += chess.sd.bfactor;
 			npos++;
-			progressbar(nlines, npos);
+			progressbar(nfen, npos);
 		} else
 			printf("\nInvalid FEN string: %s\n", fen);
 	}
-	my_close(fp, BENCH_FILE);
 
 	timer = get_ms() - timer;
 	t_elapsed = timer / 1000.0;
